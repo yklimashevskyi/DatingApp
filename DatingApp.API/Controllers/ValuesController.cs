22deac36e1ba118c06495b876357621821abf340
Yevhenii <klimashevskyi.yevhenii@gmail.com>
2019-02-14 16:32:32 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
